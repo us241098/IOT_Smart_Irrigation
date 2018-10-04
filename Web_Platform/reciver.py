8fd7876b2a20379c183d7b6e433931eabc90249e
Utsav Shukla <us241077@gmail.com>
2018-10-04 21:44:58 +0530

    Added Timestamp

--- Web_Platform/reciver.py
+++ Web_Platform/reciver.py
@@ -1,27 +1,30 @@
 import paho.mqtt.client as mqtt
+import pandas as pd
+from numpy  import array
+from time import gmtime, strftime
 
-MQTT_SERVER = "192.168.43.3"
+
+MQTT_SERVER = "localhost"
 MQTT_PATH = "test_channel"
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+#    print("Connected with result code "+str(rc))
     client.subscribe(MQTT_PATH)
 
 def on_message(client, userdata, msg):
     msg = msg.payload
-    print msg
+#    print msg
     write_csv(msg)
 
 def write_csv(msg):
-    sensor, value = msg.split('-')
-    lines = []
-
-    with open('recived.txt') as f:
-        lines = f.read().splitlines()
-    lines[int(sensor)] = value
-    with open('recived.txt','w') as f:
-        for line in lines:
-            f.write("%s\n" % line)
+	df=pd.read_csv('read.csv')
+	msg=msg.split()
+	a = array(msg)
+#	print a[0]
+	df.loc[df['sensor'] == int(a[0]), 'readings'] = a[1]
+	df.loc[df['sensor'] == int(a[0]), 'timestamp'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+	df.to_csv('read.csv', encoding='utf-8', index=False)
+	
 
 
 client = mqtt.Client()
